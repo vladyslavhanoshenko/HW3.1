e182c065d1e468a659688db72d8f5db65672a6b8
Vladyslav Hanoshenko1 <Vladyslav_Hanoshenko1@epam.com>
2019-03-13 15:02:12 +0200

    without formatting

---
+++ HW3.1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace HW3._1
+{
+    class Program
+    {
+        public static List<int> items = new List<int>();
+
+        public static void ShortList()
+        {
+            if (items.Count() >= 2)
+            {
+                Console.WriteLine("Your short list is:");
+                Console.WriteLine($"{items[0]} {items[items.Count - 1]}");
+            }
+            else
+            {
+                ShortList();
+            }
+        }
+        public static void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void DescendOrder()
+        {
+            Console.WriteLine("Your sorted descending list is: ");
+            items.Sort();
+            items.Reverse();
+            ShowList();
+        }
+        public static void Run()
+        {
+            Console.WriteLine("Please enter int items to the list(enter 'stop' to finish:)");
+            while (true)
+            {
+                string inputString = Console.ReadLine();
+                if (inputString == "stop")
+                {
+                    break;
+                }
+
+                var isInt = int.TryParse(inputString, out var digit);
+                if (!isInt)
+                {
+                    Console.WriteLine("Entered value is not an int. Please enter a number: ");
+                    continue;
+                }
+                else
+                {
+                    items.Add(digit);
+                }
+            }
+            if (items.Count == 0)
+            {
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Your list is:");
+            ShowList();
+        }
+        static void Main(string[] args)
+        {
+            Run();
+            DescendOrder();
+            ShortList();
+
+            Console.ReadKey();
+        }
+    }
+}
+
